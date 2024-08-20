e99d123b54117f75c3934b2f6b9f064609c25878
MarcoCipolletta <marcocipolletta1998@gmail.com>
2024-08-20 16:37:49 +0200

    es13

--- D2.js
+++ D2.js
@@ -178,7 +178,6 @@ const me = {
 /* SCRIVI QUI LA TUA RISPOSTA */
 
 me.city = "toronto";
-
 console.log(me);
 
 /* ESERCIZIO 12
@@ -195,6 +194,8 @@ console.log(me);
 */
 
 /* SCRIVI QUI LA TUA RISPOSTA */
+me.skills.pop();
+console.log(me);
 
 /* ESERCIZIO 14
   Scrivi del codice per creare un array inizialmente vuoto. Riempilo successivamente con i numeri da 1 a 10.
